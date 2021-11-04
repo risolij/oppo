317d9116cced057d5a8b4b68dfc6ad1f18bb7b95
risolij <risolij@gmail.com>
2021-11-03 21:15:10 -0700

    updated wrappers

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import sys
 from oppo import Oppo
+import time
 
 
+def timeit(func):
+    def wrapper():
+        time_start = time.time()
+        func()
+        time_end = time.time()
+        print(f"Function {__name__}:: Took {time_end} seconds")
+    return wrapper
 
+
+def banner(func):
+    def wrapper():
+        print("-" * 50)
+        func()
+        print("-" * 50)
+    return wrapper
+
+
+@timeit
+@banner
 def main():
     oppo = Oppo(sys.argv[1], sys.argv[2], sys.argv[3]);
     oppo.scan()
