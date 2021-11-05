9a667379214b7cb2c958f914003dd7616419f11b
risolij <risolij@gmail.com>
2021-11-05 09:38:25 -0700

    moving function call

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from lib import *
 @timeit
 @banner
 def main():
-    is_three_args()
     oppo = Oppo(sys.argv[1], sys.argv[2], sys.argv[3]);
     oppo.scan()
 
 
 if __name__ == "__main__":
+    is_three_args()
     main()
